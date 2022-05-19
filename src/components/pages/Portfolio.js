554f19361ddadfbb6c1469332beb09023fd2d4dc
LisaCR01 <lisagunn01@gmail.com>
2022-05-19 16:17:52 +0100

    added header and footer

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,12 +1,9 @@
 import React from 'react';
 
-export default function AboutMe() {
+export default function Portfolio() {
   return (
     <div>
-      <h1>About Me</h1>
-      <p>
-      I’m an accomplished digital artist with a background in both STEM and art related fields; embarking on a design career in both physical and virtual spaces. I have experience in frontend and backend development, with a particular interest in using CSS frameworks to create aesthetic applications. My knowledge within web development means I am able to create fully responsive applications with polished interfaces. I will complete the Birmingham coding bootcamp in May.I’m based in the Midlands, and open to remote part-time working, to fit in with my architectural studies.
-      </p>
+        portfolio
     </div>
   );
 }

cf71baa6b3fcd0212dc811164ca2ceb19f250d6a
LisaCR01 <lisagunn01@gmail.com>
2022-05-20 15:35:07 +0100

    made icons at bottom a better size

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,16 +5,16 @@ import { BsLinkedin } from "react-icons/bs";
 
 function Footer() {
   return (
-<footer className="container m-0 text-center">
+<footer className="container m-0 text-center ">
     <div className="row">
         <div className="col-md-4">
-        <a className="link-secondary" href="https://github.com/LisaCR01">
-            <BsGithub/>
+        <a className="link-secondary fa-lg p-4" href="https://github.com/LisaCR01">
+            <BsGithub size={50}/>
          </a>
         </div>
          <div className="col-md-4 ms-auto">
          <a className="link-secondary" href="https://www.linkedin.com/in/LisaCR01">
-             <BsLinkedin/>
+             <BsLinkedin size={50}/>
          </a>
          </div>
     </div>
